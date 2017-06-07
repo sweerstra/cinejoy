b05a87a980404cb9597f6bba9ec697f9e4d750d9
Sjoerd Weerstra <SJOERD@SGRNET.local>
2017-06-07 17:14:35 +0200

    Basic html fetch

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express'),
-    fs = require('fs'),
-    path = require('path'),
-    app = express();
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var fetch = require('node-fetch');
+var app = express();
 
 Object.assign = require('object-assign');
 app.use(express.static(path.join(__dirname, 'views')));
@@ -13,8 +14,25 @@ app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.listen(port, ip);
+app.get('/test', function (req, res) {
+    var url = 'https://www.euroscoop.nl/tilburg/films/';
+    scrapeHtml(url).then(function (html) {
+        console.log(html.querySelectorAll('.instafilta-target'));
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
 
+function scrapeHtml(url) {
+    return fetch(url).then(function (res) {
+        var html = res.text();
+        var el = document.createElement('html');
+        el.innerHTML = html;
+        return el;
+    });
+}
+
+app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app;
