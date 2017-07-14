75073b6aaf24456247fab605f34f29c27427eade
RussianDaddy <sjoerd.weerstra@hotmail.com>
2017-07-14 22:34:25 +0200

    Cleaned up scheduling

--- services/schedule.service.js
+++ services/schedule.service.js
@@ -2,26 +2,14 @@ const cheerio = require('cheerio');
 const request = require('../data/index.js');
 
 module.exports = {
-    getCorrectTimeLink(link, minTime) {
-        return getScheduleScript(link).then((schedules) => {
-            for (var i = 0; i < schedules.length; i++) {
-                const schedule = schedules[i];
-                const scheduleName = schedule.name;
-
-                if (SCHEDULE_DAYS.some(day => scheduleName.indexOf(day) !== -1)) {
-                    const checkoutLink = getOptimizedCheckoutLink(schedule, minTime || 1900)
-                    return Promise.resolve(createReservationLink(checkoutLink));
-                }
-            }
-        });
-    },
-
     getScheduleData(link) {
-        return getScheduleScript(link).then((data) => {
+        return findScheduleScript(link).then((data) => {
             const schedule = [];
             data.forEach((day) => {
+                if (day.active.every(a => a === 0)) return;
+
                 const timeslots = day.timeslots;
-                const obj = { name: /*cutDay(*/day.name/*)*/, data: [] };
+                const obj = { name: day.name, data: [] };
                 for (var i = 0; i < timeslots.length; i++) {
                     if (day.active[i] === 1 && day.full[i] === 0) {
                         obj.data.push({
@@ -38,28 +26,13 @@ module.exports = {
     }
 };
 
-const SCHEDULE_DAYS = ['vrijdag', 'zaterdag', 'maandag'];
-
-function getOptimizedCheckoutLink (schedule, minTime) {
-    const timeslots = schedule.timeslots.reverse();
-    const length = timeslots.length;
-
-    for (var i = 0; i < timeslots.length; i++) {
-        const time = +timeslots[i].replace(':', '');
-
-        if (time > minTime) {
-            return schedule.links[length - i - 1];
-        }
-    }
-}
-
 function createReservationLink (url) {
     if (!url) return null;
     const pop = url.split('/').pop();
     return `http://tilburg.euroscoop.nl/Reservation.asp?WCI=templateLogin&WCE=&Vorst=${pop}&JAVA=true`;
 }
 
-function getScheduleScript (url) {
+function findScheduleScript (url) {
     const KEYWORD = 'scheduleData';
     return request.getHtml(url).then((html) => {
         const $ = cheerio.load(html);
@@ -72,8 +45,4 @@ function getScheduleScript (url) {
             }
         }
     });
-}
-
-function cutDay (day) {
-    return day.split(' ').slice(1).join(' ');
 }
